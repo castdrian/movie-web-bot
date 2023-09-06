84684a182913d1838815b2e6e2fffdd0d5e2faf9
castdrian <adrifcastr@gmail.com>
2023-09-06 22:02:45 +0200

    feat: more tags setup

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
 import { createConfigLoader } from 'neat-config';
+import TOML from '@ltd/j-toml';
 import { z } from 'zod';
+import { Collection } from 'discord.js';
+import { readFileSync } from 'node:fs';
 
 const schema = z.object({
 	discordToken: z.string(),
@@ -10,3 +13,14 @@ const schema = z.object({
 const prefix = 'CONF_';
 
 export const config = createConfigLoader().addFromFile('.env', { prefix }).addFromEnvironment(prefix).addZodSchema(schema).load();
+
+interface TagStore {
+	tags: Record<string, string>;
+}
+
+const tagStore = TOML.parse(readFileSync('./src/tags.toml', 'utf8')) as unknown as TagStore;
+export const tagCache = new Collection<string, string>();
+
+for (const [key, value] of Object.entries(tagStore.tags)) {
+	tagCache.set(key, value);
+}
